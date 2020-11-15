fcafa97a768fab6c4b8aa59e3632da3594533bfc
kolya <kovalyov.k51@gmail.com>
2020-11-16 01:51:21 +0300

    [TASK-5] odd method

--- import-service/importfileparser/importfileparser.js
+++ import-service/importfileparser/importfileparser.js
@@ -3,23 +3,6 @@ const { S3 } = require("aws-sdk");
 const { BUCKET, REGION } = require("../constants");
 const csv = require("csv-parser");
 
-processRecord = async (Key, s3) => {
-  const params = {
-    Bucket: BUCKET,
-    Key,
-  };
-
-  const s3Stream = s3.getObject(params).createReadStream();
-
-  s3Stream
-    .pipe(csv())
-    .on("data", (data) => {
-      console.log(JSON.stringify(data, null, 4));
-    })
-    .on("end", resolve)
-    .on("error", reject);
-};
-
 module.exports.importFileParser = async (event) => {
   try {
     const s3 = new S3({ region: REGION });
