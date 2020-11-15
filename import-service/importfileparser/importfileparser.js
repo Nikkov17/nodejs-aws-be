9c6ec81983a4b7b70e87e5d5cbab27db20659599
kolya <kovalyov.k51@gmail.com>
2020-11-16 02:17:41 +0300

    [TASK-5] fix async

--- import-service/importfileparser/importfileparser.js
+++ import-service/importfileparser/importfileparser.js
@@ -3,16 +3,16 @@ const { S3 } = require("aws-sdk");
 const { BUCKET, REGION } = require("../constants");
 const csv = require("csv-parser");
 
-module.exports.importFileParser = async (event) => {
+module.exports.importFileParser = (event) => {
   try {
     const s3 = new S3({ region: REGION });
     const { Records } = event;
 
     for (const record of Records) {
-      const key = record.s3.object.key;
+      const Key = record.s3.object.key;
       const params = {
         Bucket: BUCKET,
-        key,
+        Key,
       };
 
       const s3Stream = s3.getObject(params).createReadStream();
