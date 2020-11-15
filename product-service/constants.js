6e2c02209a21d273f9859496364d645f13ac57c4
kolya <kovalyov.k51@gmail.com>
2020-11-16 00:42:14 +0300

    [TASK-4] create constants

---
+++ product-service/constants.js
@@ -0,0 +1,15 @@
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+module.exports.constants = {
+  DBOPTIONS: {
+    host: PG_HOST,
+    port: PG_PORT,
+    database: PG_DATABASE,
+    user: PG_USERNAME,
+    password: PG_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    connectionTimeoutMillis: 5000,
+  },
+};
--- product-service/createnewproducthandler/createnewproducthandler.js
+++ product-service/createnewproducthandler/createnewproducthandler.js
@@ -1,17 +1,7 @@
 "use strict";
 const { Client } = require("pg");
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
-const dbOptions = {
-  host: PG_HOST,
-  port: PG_PORT,
-  database: PG_DATABASE,
-  user: PG_USERNAME,
-  password: PG_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeoutMillis: 5000,
-};
+const { DBOPTIONS } = require("../constants");
+const dbOptions = DBOPTIONS;
 
 module.exports.createNewProduct = async (event) => {
   const client = new Client(dbOptions);
