96ed8df346938071cb6bda4966c405499bd055d3
kolya <kovalyov.k51@gmail.com>
2020-11-23 08:04:31 +0300

    [TASK-6] add fucking filterPolicy

--- product-service/catalogbatchprocess/catalogbatchprocess.js
+++ product-service/catalogbatchprocess/catalogbatchprocess.js
@@ -1,24 +1,26 @@
 "use strict";
 const { SNS } = require("aws-sdk");
 
-module.exports.catalogBatchProcess = (event) => {
+module.exports.catalogBatchProcess = async (event) => {
   const products = event.Records.map((record) => record.body);
   console.log(`products: ${products}`);
 
   const sns = new SNS({ region: "eu-west-1" });
 
-  sns.publish(
-    {
-      Subject: "Product created",
-      Message: `Your products has been created: ${products}`,
-      TopicArn: process.env.SNS_ARN,
-    },
-    (error) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-      } else {
-        console.log(`Send email about products creation: ${products}`);
+  await sns
+    .publish(
+      {
+        Subject: "Product created",
+        Message: `Your products has been created: ${products}`,
+        TopicArn: process.env.SNS_ARN,
+      },
+      (error) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+        } else {
+          console.log(`Send email about products creation: ${products}`);
+        }
       }
-    }
-  );
+    )
+    .promise();
 };
